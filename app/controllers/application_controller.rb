fe262c508ef379650bd8e0c1a2c9504c243695fc
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-06-15 15:37:44 -0300

    Create Authentication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,12 +14,9 @@ class ApplicationController < ActionController::API
     end
   end
 
-  SECRET_KEY = Rails.application.secrets.secret_key_base. to_s
+  private
 
-  def encode(payload, exp = 24.hours.from_now)
-    payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
-  end
+  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
 
   def decode(token)
     decoded = JWT.decode(token, SECRET_KEY)[0]
---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,24 @@
+class AuthenticationController < ApplicationController
+  skip_before_action :autorize_request
+
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user&.authenticate(params[:password])
+      token = encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { token: token, exp: time.strftime('%m-%d-%Y %H:%M'),
+                     email: @user.email }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+
+  def encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+end
