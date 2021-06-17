3e7b544872febaf63976ee6798925f0e34c00b26
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-06-17 17:33:27 -0300

    Validate title uniqueness on posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :category
   belongs_to :user
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :content, presence: true
   validates :img_url, format: URI::DEFAULT_PARSER.make_regexp(%w[http https]), allow_blank: true
 end
