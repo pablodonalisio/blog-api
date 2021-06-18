9cfea67dbe79ad3562fd88316f5cdee793029e14
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-06-18 14:49:01 -0300

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'posts#index'
   post '/auth/sign_up', to: 'users#create'
   post '/auth/login', to: 'authentication#login'
   resources :posts, only: %i[index show create update destroy]
